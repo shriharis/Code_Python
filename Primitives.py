10c87174e37c25f74911d5def9247160e142f5fe
shrinivas <shriharis1729@gmail.com>
2020-10-19 10:15:06 +0530

    primitive types

---
+++ Primitives.py
@@ -0,0 +1,17 @@
+#primitives
+
+var1 = "python"
+var2 = 'python'
+var3 = "I'm coding"
+var4 = 1.61803398875
+var5 = -127
+var6 = False
+
+print(
+    '\nvar1',var1,'| type of var1 ',type(var1), 
+    '\nvar2',var2,'| type of var2 ',type(var2),
+    '\nvar3',var3,'| type of var3 ',type(var3),
+    '\nvar4',var4,'| type of var4 ',type(var4),
+    '\nvar5',var5,'| type of var5 ',type(var5),
+    '\nvar6',var6,'| type of var6 ',type(var6)
+)
